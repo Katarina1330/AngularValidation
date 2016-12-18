7be0a5b7e2bca6a4bd8a6502d82df1fe1acfe74e
Katarina1330 <Katarina@Katarinas-MBP.AN-700-AP-I-AC>
2016-12-18 15:33:16 -0800

    Improved ng-messages

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var app = angular.module('myApp', ['ngMessages']);
 app.controller('registrationController', function ($scope) {
-    var person = { firstName: '', lastName: '', email: '', password: '' };
-
-    $scope.person = person;
+    $scope.person = {};
 
     $scope.submitForm = function(form) {
         console.log($scope.person);
