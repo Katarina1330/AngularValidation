afc97cbf8043fbe35f6ec352ce59307cc862a803
Katarina1330 <Katarina@Katarinas-MacBook-Pro.local>
2016-12-18 01:30:00 -0800

    Added ng-messages and validations

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngMessages']);
 app.controller('registrationController', function ($scope) {
     var person = { firstName: '', lastName: '', email: '', password: '' };
 
     $scope.person = person;
 
-    $scope.submitForm = function() {
+    $scope.submitForm = function(form) {
         console.log($scope.person);
     }
 });
